3d81082472da6bcd0ec91422fdc36dcf393df528
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-16 22:56:34 +0200

    refactor(components.hideoutsideclick): move to telecomUniverseComponents

--- client/telecomUniverseComponents/index.js
+++ client/telecomUniverseComponents/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 
 import tucChartjs from './chartjs';
+import tucHideOutsideClick from './hideOutsideClick';
 import tucUnitHumanize from './unit/humanize';
 
 export default angular
   .module('telecomUniverseComponents', [
     tucChartjs,
+    tucHideOutsideClick,
     tucUnitHumanize,
   ])
   .name;
