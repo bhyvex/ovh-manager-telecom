8b26aed4253df89e8bbf5b2510a08d331b969f1d
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-17 10:36:46 +0200

    refactor(components.custom-asterisk): move to telecomUniverseComponents

--- client/telecomUniverseComponents/index.js
+++ client/telecomUniverseComponents/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 
 import tucChartjs from './chartjs';
+import tucCustomAsterisk from './custom-asterisk';
 import tucUnitHumanize from './unit/humanize';
 
 export default angular
   .module('telecomUniverseComponents', [
     tucChartjs,
+    tucCustomAsterisk,
     tucUnitHumanize,
   ])
   .name;
