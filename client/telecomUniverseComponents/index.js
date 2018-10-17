649c8282d1f4ac1ef7749ea3f433949b31dd4a25
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-10-17 11:36:35 +0200

    refactor(components.filters): move to telecomUniverseComponents

--- client/telecomUniverseComponents/index.js
+++ client/telecomUniverseComponents/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 
 import tucChartjs from './chartjs';
+import tucFilters from './filters';
 import tucUnitHumanize from './unit/humanize';
 
 export default angular
   .module('telecomUniverseComponents', [
     tucChartjs,
+    tucFilters,
     tucUnitHumanize,
   ])
   .name;
